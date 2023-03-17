2f81795607da6fe658ae6c0697d683f9efce2079
kaique <kaiquevilasbs@gmail.com>
2023-03-17 09:33:26 -0300

    Estilizar navbar e adcionar footer

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,24 +1,41 @@
 class MobileNavbar {
-    constructor (mobileMenu, navList, navLinks){
-       this.mobileMenu = document.querySelector(mobileMenu);
-       this.navList = document.querySelector(navList);
-       this.navLinks = document.querySelectorAll(navLinks );
-       this.activeClass = "active";
+    constructor (mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector (mobileMenu);
+        this.navList = document.querySelector (navList);
+        this.navLinks = document.querySelectorAll (navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+          link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+              }s`);
+        });
+      }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
-      this.mobileMenu.addEventListener("click", () => console.log("oi"));  
+        this.mobileMenu.addEventListener ("click", this.handleClick);
     }
-
     init() {
-        if (this.mobileMenu) {
+      if (this.mobileMenu) {
             this.addClickEvent();
-        }
-        return this;
+      }
+      return this;
     }
 }
 
-const mobileNavbar = new MobileNavbar (
+const mobileNavbar = new MobileNavbar(
 ".mobile-menu",
 ".nav-list",
 ".nav-list p",
